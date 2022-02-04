1d4e1b15ae8aef45b5e28094464ff91207739154
yahyasipahi <yahyasipahi@hotmail.com>
2022-02-04 22:52:45 +0100

    Komet isteniyor

---
+++ Yeniproje.py
@@ -0,0 +1,10 @@
+#  Python projesi denemesi icin hazirlanan bir fayl
+print('Adinizi giriniz ')
+ad = ''
+ad = input()
+print(ad);
+print()
+print('Baslik')
+print('Helo wold')
+print()
+print('baravo! '  + ad + ' nin ilk programi')
